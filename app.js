c070b8747fad9fe0c314992b3b787b6f8f2bcb36
RojoRandy <rojo.q.randy@gmail.com>
2021-05-04 13:54:18 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require('colors');
+const {mostrarMenu, pausa} = require('./helpers/mensajes')
+
+const main = async()=>{
+    console.log("Hola Mundo");
+
+    let opt='';
+    do{
+        opt = await mostrarMenu();
+        console.log({opt})
+        await pausa();
+    }while(opt!=='0')
+
+    pausa();
+}
+
+main();
